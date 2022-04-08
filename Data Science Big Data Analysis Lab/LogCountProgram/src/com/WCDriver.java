5575c3f3e68c6db1c1f22d227837dc308eb11688
samueldenzil <denzil.s.samuel@gmail.com>
2022-04-08 20:52:41 +0530

    updated WCDriver.java

--- Data Science Big Data Analysis Lab/LogCountProgram/src/com/WCDriver.java
+++ Data Science Big Data Analysis Lab/LogCountProgram/src/com/WCDriver.java
@@ -2,9 +2,13 @@
 
 package com;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
@@ -13,10 +17,10 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class WCDriver {
-
+	
 	public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
 		Configuration config = new Configuration();
-		Job job = Job.getInstance(config, "log count");
+		Job job = Job.getInstance(config, "word count");
 		job.setJarByClass(com.WCDriver.class);
 		job.setMapperClass(com.WCMap.class);
 		job.setReducerClass(com.WCReduce.class);
@@ -24,6 +28,28 @@ public class WCDriver {
 		job.setOutputValueClass(IntWritable.class);
 		FileInputFormat.addInputPath(job, new Path(args[0]));
 		FileOutputFormat.setOutputPath(job, new Path(args[1]));
-		System.exit(job.waitForCompletion(true) ? 0 : 1);
+		job.waitForCompletion(true);
+				
+
+		FileSystem fs = FileSystem.get(config);
+		FSDataInputStream in = fs.open(new Path("hdfs://localhost:9000" + args[1] + "/part-r-00000"));
+		BufferedReader d = new BufferedReader(new InputStreamReader(in));
+		
+		int maxCount = 0;
+		String ipAddress = "";
+		String line = null;		
+		
+		while ((line = d.readLine()) != null) {
+			String[] words = line.split("\t");
+			int currentCount = Integer.parseInt(words[1]);
+			if (currentCount > maxCount) {
+				maxCount = currentCount;
+				ipAddress = words[0];
+			}
+		}
+		
+		System.out.println();
+		System.out.println("Maximum logged-in IP Address is: " + ipAddress);
+		System.out.println("For " + maxCount + " times");
 	}
 }
